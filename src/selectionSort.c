9701ed0f9871ffb5a3a1c11498d16d6c73571fcd
ThaysonScript <thaysonguedes01@gmail.com>
2024-08-23 15:18:55 -0300

    algumas implementações iniciais adicionadas

--- src/selectionSort.c
+++ src/selectionSort.c
@@ -2,6 +2,26 @@
 #include "../includes/selectionSortHeader.h"
 #include "../includes/principalHeader.h"
 
+void criar() {
+    
+}
+
+void ler() {
+    
+}
+
+void atualizar() {
+    
+}
+
+void deletar() {
+    
+}
+
+void ordenarPorSelection() {
+
+}
+
 void selectionSort() {
     printf("algo aqui!");
 }
