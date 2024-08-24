066445a76a3e9560f842b18c714048ebe7619f49
ThaysonScript <thaysonguedes01@gmail.com>
2024-08-24 16:08:45 -0300

    implementando operacoes de crud e algumas modificacoes em cabecalhos e alocacoes

--- includes/insertionSortHeader.h
+++ includes/insertionSortHeader.h
@@ -18,21 +18,35 @@ struct FuncionariosInsertion {
 /** @brief Alocar estrutura de FuncionariosInsertion
  *  @param struct FuncionariosInsertion *funcionario
  *  @return endereco para nova estrutura alocada*/
-struct FuncionariosInsertion *inicializarFuncionarioInsertion(struct FuncionariosInsertion *funcionarioInserction);
+void alocarFuncionarioInsertion(struct FuncionariosInsertion** novoFuncionario, int* qtdFuncionario);
+
+/** @brief Verificar alocacao */
+void verificarAlocacao(struct FuncionariosInsertion** funcionarioInsertion);
 
 /** @brief Desalocar estrutura de FuncionariosInsertion
  *  @param struct FuncionariosInsertion *funcionario
  *  @return nao retorna nada*/
-void desalocarFuncionarioInsertion(struct FuncionariosInsertion *funcionarioInserction);
+void desalocarFuncionarioInsertion(struct FuncionariosInsertion** funcionarioInserction);
 
 /** Operacoes de C.R.U.D */
-void criarInsertion();
-void lerInsertion();
-void atualizarInsertion();
-void deletarInsertion();
+int criarFuncionarioInsertion(struct FuncionariosInsertion** novoFuncionario, int* qttFuncionario);
+void lerFuncionarioInsertion(struct FuncionariosInsertion** funcionarios, int* qttFuncionario);
+void atualizarFuncionarioInsertion(struct FuncionariosInsertion** funcionarios, int* qttFuncionario);
+void deletarFuncionarioInsertion(struct FuncionariosInsertion** funcionarios, int* qttFuncionario);
 
-
+// algoritmo para ordenar
 void ordenarPorInsertion();
+
+// limpar termianl
+void limparTela();
+
+// chamada principal
 void insertionSort();
 
+#endif
+
+#ifdef _WIN32
+    #define COMANDO_LIMPAR "cls"
+#else
+    #define COMANDO_LIMPAR "reset"
 #endif
--- src/menus.c
+++ src/menus.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "../includes/menus.h"
 
-int menuPrincipal() {
+int menuPrincipalInsertion() {
+    int escolha;
+
     puts("\t\t\t\t\t\t---------------- GERENCIAR FUNCIONARIOS --------------");
     puts("\t\t\t\t\t\t--------- C.R.U.D");
     puts("\t\t\t\t\t\t[1] - CRIAR FUNCIONARIO");
@@ -12,12 +14,12 @@ int menuPrincipal() {
     puts("\t\t\t\t\t\t-------- DADOS DE DESEMPENHO DE ALGORITMO");
     puts("\t\t\t\t\t\t[5] - REALIZAR PRE DADOS");
     puts("");
-    puts("\t\t\t\t\t\t--------- ORDENACAO");
+    puts("\t\t\t\t\t\t--------- ORDENAR");
     puts("\t\t\t\t\t\t[6] - INSERTION SORT");
-    puts("\t\t\t\t\t\t[7] - SELECTION");
+    puts("");
+    puts("\t\t\t\t\t\t[8] -------- VOLTAR");
     puts("\t\t\t\t\t\t------------------------------------------------------");
 
-    int escolha;
     printf("\t\t\t\t\t\tEscolha a entrada: ");
     scanf("%i", &escolha);
 
@@ -49,12 +51,78 @@ int menuPrincipal() {
         case 7:
             puts("escolheu 7");
             return escolha;
+
+        case 8:
+            puts("escolheu 8");
+            return escolha;
     
         default:
             puts("invalido ou desejou sair");
             break;
     }
 }
+
+int menuPrincipalSelection() {
+    int escolha;
+
+    puts("\t\t\t\t\t\t---------------- GERENCIAR FUNCIONARIOS --------------");
+    puts("\t\t\t\t\t\t--------- C.R.U.D");
+    puts("\t\t\t\t\t\t[1] - CRIAR FUNCIONARIO");
+    puts("\t\t\t\t\t\t[2] - LER FUNCIONARIO");
+    puts("\t\t\t\t\t\t[3] - ATUALIZAR FUNCIONARIO");
+    puts("\t\t\t\t\t\t[4] - DELETAR FUNCIONARIO");
+    puts("");
+    puts("\t\t\t\t\t\t-------- DADOS DE DESEMPENHO DE ALGORITMO");
+    puts("\t\t\t\t\t\t[5] - REALIZAR PRE DADOS");
+    puts("");
+    puts("\t\t\t\t\t\t--------- ORDENAR");
+    puts("\t\t\t\t\t\t[7] - SELECTION SORT");
+    puts("");
+    puts("\t\t\t\t\t\t[8] -------- VOLTAR");
+    puts("\t\t\t\t\t\t------------------------------------------------------");
+
+    printf("\t\t\t\t\t\tEscolha a entrada: ");
+    scanf("%i", &escolha);
+
+    switch (escolha) {
+        case 1:
+            puts("escolheu 1");
+            return escolha;
+
+        case 2:
+            puts("escolheu 2");
+            return escolha;
+
+        case 3:
+            puts("escolheu 3");
+            return escolha;
+
+        case 4:
+            puts("escolheu 4");
+            return escolha;
+
+        case 5:
+            puts("escolheu 5");
+            return escolha;
+
+        case 6:
+            puts("escolheu 6");
+            return escolha;
+
+        case 7:
+            puts("escolheu 7");
+            return escolha;
+
+        case 8:
+            puts("escolheu 8");
+            return escolha;
+    
+        default:
+            puts("invalido ou desejou sair");
+            break;
+    }
+}
+
 
 int desejaGerarDados() {
     int gerar;
@@ -71,9 +139,8 @@ int desejaGerarDados() {
     printf("\t\t\t\t\t\tEscolha a entrada: ");
     scanf("%i", &gerar);
 
-    while (1) {
-        switch (gerar) {
-            case 1:
+    switch (gerar) {
+        case 1:
             puts("escolheu 1");
             return gerar;
 
@@ -97,11 +164,39 @@ int desejaGerarDados() {
             puts("escolheu 6");
             return gerar;
 
-            default:
-                gerar = 0;
-                return gerar;
-        }
+        default:
+            gerar = 0;
+            return gerar;
+    }
+}
 
-        
+int escolherAlgoritmo() {
+    int escolher;
+
+    puts("\t\t\t\t\t\t-------------- ESCOLHER ALGORITMO ---------------");
+    puts("\t\t\t\t\t\t[1] - INSERTION SORT");
+    puts("\t\t\t\t\t\t[2] - SELECTION SORT");
+    puts("\t\t\t\t\t\t[3] - SAIR");
+    puts("\t\t\t\t\t\t------------------------------------------------------");
+
+    printf("\t\t\t\t\t\tEscolha a entrada: ");
+    scanf("%i", &escolher);
+
+    switch (escolher) {
+        case 1:
+            puts("escolheu 1");
+            return escolher;
+
+        case 2:
+            puts("escolheu 2");
+            return escolher;
+
+        case 3:
+            puts("escolheu 3");
+            return escolher;
+
+        default:
+            escolher = 0;
+            return escolher;
     }
 }
